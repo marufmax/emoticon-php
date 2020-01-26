0137de4244650ef6d86744b1f2ebdc82e3f23873
Maruf Alom <desk.maruf@gmail.com>
2020-01-27 01:58:54 +0600

    test: get method is tested

--- tests/EmoticonTest.php
+++ tests/EmoticonTest.php
@@ -3,10 +3,16 @@
 
 namespace MarufMax\Emoticon\Test;
 
-
+use MarufMax\Emoticon\Emoticon;
 use PHPUnit\Framework\TestCase;
 
 class EmoticonTest extends TestCase
 {
-    
+    /** @test */
+    public function it_can_return_an_emoji_when_given_a_string()
+    {
+        $emoji = new Emoticon();
+        
+        $this->assertSame('😆', $emoji->get(':laughing:'));
+    }
 }
