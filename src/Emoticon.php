ea3969b03dbe07836690e8cbdd3a32acc4e11471
Maruf Alom <desk.maruf@gmail.com>
2020-01-26 19:38:44 +0000

    Apply fixes from StyleCI

--- src/Emoticon.php
+++ src/Emoticon.php
@@ -5,32 +5,34 @@ namespace MarufMax\Emoticon;
 class Emoticon
 {
     private $list;
-    
+
     public function __construct()
     {
-        $this->list = require('IconList.php');
+        $this->list = require 'IconList.php';
     }
-    
+
     /**
-     * Get a single emoji
+     * Get a single emoji.
      *
      * @param string $name
+     *
      * @return bool|mixed
      */
     public function get(string $name)
     {
-      $itemName = $this->stripColons($name);
-      if (array_key_exists($itemName, $this->list)) {
-          return $this->list[$itemName];
-      }
-      
-      return false;
+        $itemName = $this->stripColons($name);
+        if (array_key_exists($itemName, $this->list)) {
+            return $this->list[$itemName];
+        }
+
+        return false;
     }
-    
+
     /**
-     * Replace all :emoji: with the actual emoji
+     * Replace all :emoji: with the actual emoji.
      *
      * @param string $text
+     *
      * @return string
      */
     public function emojify(string $text): string
@@ -39,36 +41,37 @@ class Emoticon
             return $this->get($match[1]);
         }, $text);
     }
-    
+
     /**
-     * Returns array of items with matching emoji
+     * Returns array of items with matching emoji.
      *
      * @param string $emoji
+     *
      * @return array
      */
     public function search(string $emoji): array
     {
         return array_filter($this->list, function ($item) use ($emoji) {
-            return strpos($item, $emoji) !== false ;
+            return strpos($item, $emoji) !== false;
         }, ARRAY_FILTER_USE_KEY);
     }
-    
+
     /**
-     * Generate a random emoji
+     * Generate a random emoji.
      */
     public function random()
     {
         return $this->list[array_rand($this->list)];
     }
-    
-    protected function stripColons(string $item) : string
+
+    protected function stripColons(string $item): string
     {
         $colonPosition = strpos($item, ':');
-        
+
         if ($colonPosition > -1) {
             return str_replace(':', '', $item);
         }
-        
+
         return $item;
     }
 }
